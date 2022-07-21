fa947c5b7bb4ad442326dc171aa33b573f54cb43
TrisTristy <103004927+TrisTrsity@users.noreply.github.com>
2022-07-21 13:56:15 -0400

    testing heroku

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const { notes } = require('./db/db');
 const fs = require('fs');
 const path = require('path')
 
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+
 function createNewNote(body, notesArray) {
     console.log(body);
     const note = body;
@@ -17,3 +25,45 @@ function createNewNote(body, notesArray) {
     // return finished code 
     return note;
   }
+  app.get('/api/notes', (req, res) => {
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
+  });
+
+
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  // add notes to json file and notes array in this function
+  const note = createNewNote(req.body, notes);
+    
+    res.json(note);
+  });
+  app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    let note;
+
+    notes.map((element, index) => {
+      if (element.id == id){
+        note = element
+        notes.splice(index, 1)
+        return res.json(note);
+      } 
+    
+    })
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
